21f9077ca248e285f58cc33e3c1d504c6aee3623
Abdelmalek LAHMAR <Abdelmalek.lahmar@gmail.com>
2015-09-06 23:31:17 +0100

    Create login form in login view

--- application/views/login.php
+++ application/views/login.php
@@ -10,6 +10,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="container">
 	<h1>Login </h1>
+	<?php
+		echo form_open('main/login_validation');
+
+		echo validation_errors();
+
+		echo "<p>Email:" ;
+		echo form_input('email');
+		echo "</p>";
+
+		echo "<p>password" ;
+		echo form_password('password');
+		echo "</p>";
+
+		echo "<p>" ;
+		echo form_submit('login_submit','Login');
+		echo "</p>";
+
+		echo form_close();
+	?>
 </div>
 
 </body>
