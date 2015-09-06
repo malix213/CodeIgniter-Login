c5db1d3e8e806533f58636076e4e1b055e769806
Abdelmalek LAHMAR <Abdelmalek.lahmar@gmail.com>
2015-09-06 23:17:36 +0100

    Creating login validation function in main controler

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,6 +4,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 	public function index()
 	{
+		$this->login();
+	}
+
+	public function login() 
+	{
 		$this->load->view('login');
+	}
+
+	public function members()
+	{
+		$this->load->view('members');
+	}
+
+	public function login_validation()
+	{
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('email','Email','required|trim|xss_clean|valid_email');
+		$this->form_validation->set_rules('password','Password','required|md5|trim');
+
+		if($this->form_validation->run()){
+			redirect('main/members');
+		} else {
+			$this->load->view('login');
+		}
+
+
 	}
 }
