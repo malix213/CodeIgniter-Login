bfbf3374904d3080acb16891fe2c66bfa44c23fc
Abdelmalek LAHMAR <Abdelmalek.lahmar@gmail.com>
2015-09-07 00:38:56 +0100

    Adding Member view

--- application/views/members.php
+++ application/views/members.php
@@ -11,6 +11,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 	<h1>Members </h1>
 	
+	<?php
+	echo "<pre>";
+	print_r($this->session->all_userdata());
+	echo "</pre>";
+	?>
+
+	<a href='<?php echo base_url()."main/logout" ?>'>Logout</a>
 </div>
 
 </body>
