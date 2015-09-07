314bd889d055b740335490b618d2b5517fb68979
Abdelmalek LAHMAR <Abdelmalek.lahmar@gmail.com>
2015-09-07 12:46:39 +0100

    Creating function add temp user  to temp_users table function

--- application/models/Model_users.php
+++ application/models/Model_users.php
@@ -3,13 +3,31 @@
 class Model_users extends CI_Model
 {
 	
-	function can_log_in()
+	public function can_log_in()
 	{
 		$this->db->where('email', $this->input->post('email'));
 		$this->db->where('password', md5($this->input->post('password')));
 		$query = $this->db->get('users');
 
 		if ($query->num_rows() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public function add_temp_user($key)
+	{
+		$data = array(
+			'email' => $this->input->post('email'), 
+			'password' => md5($this->input->post('password')),
+			'key' => $key
+		);
+
+		$query = $this->db->insert('temp_users',$data);
+
+		if ($query){
 			return true;
 		} else {
 			return false;
